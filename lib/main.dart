bd556b2b1198b76f472d22b2dc20135b256cba46
Komronbek <95031806+komronz@users.noreply.github.com>
2023-06-09 14:16:38 +0500

    making pictures deletable

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:sqlite_camera/fullImage.dart';
+import 'package:sqlite_camera/models/picture_taker.dart';
 import 'package:sqlite_camera/widgets/add_picture.dart';
 
 import 'models/provider_cam.dart';
@@ -67,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
              Padding(
                padding: const EdgeInsets.all(10.0),
                child: TextField(
-                 maxLength: 16, //updated to fix bugs
+                 maxLength: 16,
                  decoration: const InputDecoration(label: Text('Title')),
                  controller: _titleController,
                ),
@@ -84,6 +85,49 @@ class _MyHomePageState extends State<MyHomePage> {
       );
     });
   }
+  _deleteMethod({
+    required BuildContext context,
+    required int i,
+    required PictureProvider pictureTaker,
+  }) async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          elevation: 5,
+          title: Text(
+            'Warning',
+            style: TextStyle(
+                fontWeight: FontWeight.w400,
+                color: Theme.of(context).errorColor),
+          ),
+          content: const Text(
+            'Are you sure to delete?',
+            style:
+            TextStyle(fontWeight: FontWeight.w400, color: Colors.black87),
+          ),
+          actions: [
+            TextButton(
+              child: const Text('NO'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('YES'),
+              onPressed: ()  async {
+                Provider.of<PictureProvider>(context, listen: false).deleteById(pictureTaker.items[i].id.toString());
+                Provider.of<PictureProvider>(context, listen: false).getData();
+                Navigator.of(context).pop();
+                print('here');
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -101,76 +145,80 @@ class _MyHomePageState extends State<MyHomePage> {
           onPressed: () => _startAddNewTransaction(context),
           child: const Icon(Icons.add_rounded, size: 70, color: Colors.black54,),
         ),
-        body: FutureBuilder(
-          future: Provider.of<PictureProvider>(context, listen: false).getData(),
-          builder: (ctx, snapshot)
-          => snapshot.connectionState == ConnectionState.waiting 
-              ? const Center(child: CircularProgressIndicator(),) :
-          Consumer<PictureProvider>(
-            child: const Center(child: Text('You have not any pictures yet!'),),
-            builder: (ctx, pictureTaker, ch) { // ch did not work
-              return pictureTaker.items.isEmpty ? const Center(child: Text('You have not any pictures yet!'),) : GridView.builder(
-                itemCount: pictureTaker.items.length,
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 300,
-                  childAspectRatio: 1,
-                  crossAxisSpacing: 1,
-                  mainAxisSpacing: 1,
-                ),
-                itemBuilder: (ctx, i) {
-                  return InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(fullscreenDialog: true, builder: (context) => FullImage(title: pictureTaker.items[i].title, image: pictureTaker.items[i].image,)
-                      ));
-                    },
-                    child: Container(
-                      margin: EdgeInsets.all(10),
-                      width: 150,
-                      height: 150,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 10,
-                            offset: Offset(3,2),
-                            spreadRadius: 1,
-                          )
-                        ],
+        body: SizedBox(
+          height: 620,
+          child: FutureBuilder(
+            future: Provider.of<PictureProvider>(context, listen: false).getData(),
+            builder: (ctx, snapshot)
+            => snapshot.connectionState == ConnectionState.waiting
+                ? const Center(child: CircularProgressIndicator(),) :
+            Consumer<PictureProvider>(
+              child: const Center(child: Text('You have not any pictures yet!'),),
+              builder: (ctx, pictureTaker, ch) { // ch did not work
+                return pictureTaker.items.isEmpty ? const Center(child: Text('You have not any pictures yet!'),) : GridView.builder(
+                  itemCount: pictureTaker.items.length,
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 300,
+                    childAspectRatio: 1,
+                    crossAxisSpacing: 1,
+                    mainAxisSpacing: 1,
+                  ),
+                  itemBuilder: (ctx, i) {
+                    return InkWell(
+                      onLongPress: () => _deleteMethod(context: ctx, i: i, pictureTaker: pictureTaker),
+                      onTap: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(fullscreenDialog: true, builder: (context) => FullImage(title: pictureTaker.items[i].title, image: pictureTaker.items[i].image,)
+                        ));
+                      },
+                      child: Container(
+                        margin: EdgeInsets.all(10),
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 10,
+                              offset: Offset(3,2),
+                              spreadRadius: 1,
+                            )
+                          ],
+                        ),
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image(
+                                fit: BoxFit.cover,
+                                height: double.infinity,
+                                width: double.infinity,
+                                alignment: Alignment.center,
+                                image: FileImage(pictureTaker.items[i].image),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 8,
+                              right: 0,
+                              child: Container(
+                              padding: const EdgeInsets.all(8),
+                              decoration: const BoxDecoration(
+                                color: Color(0xC7031113),
+                                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(5) , topLeft: Radius.circular(5))
+                              ),
+                              child: Center(
+                                child: Text(pictureTaker.items[i].title.toUpperCase(), style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      child: Stack(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image(
-                              fit: BoxFit.cover,
-                              height: double.infinity,
-                              width: double.infinity,
-                              alignment: Alignment.center,
-                              image: FileImage(pictureTaker.items[i].image),
-                            ),
-                          ),
-                          Positioned(
-                            bottom: 8,
-                            right: 0,
-                            child: Container(
-                            padding: const EdgeInsets.all(8),
-                            decoration: const BoxDecoration(
-                              color: Color(0xC7031113),
-                              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(5) , topLeft: Radius.circular(5))
-                            ),
-                            child: Center(
-                              child: Text(pictureTaker.items[i].title.toUpperCase(), style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              );
-            }
+                    );
+                  },
+                );
+              }
+            ),
           ),
         )
       );
